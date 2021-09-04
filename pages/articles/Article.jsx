76e93380cfb1033a3e565a6f51fde16f5cd663ff
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2021-09-04 15:51:34 +0100

    fix grid item + add adress to single article

--- pages/articles/Article.jsx
+++ pages/articles/Article.jsx
@@ -1,10 +1,18 @@
-const Article = ({ article }) => {
-  const { title, urlToImage, description } = article;
+const Article = ({ article, id }) => {
+  const { title, urlToImage } = article;
+  const URL = `/articles/${id}/${title}`;
   return (
-    <article className="w-full p-4 bg-gray-100 border-1 border-gray-500 shadow">
-      <img src={urlToImage} alt={title} />
-      <h1 className="my-4">{title}</h1>
-      <p>{description}</p>
+    <article className="h-full bg-gray-100 shadow-md transition duration-700 ease hover:bg-gray-200">
+      <a href={URL} className="flex flex-col h-full">
+        <img
+          className="w-full h-full"
+          src={
+            urlToImage || "https://via.placeholder.com/900?text=Image+Not+Found"
+          }
+          alt={title}
+        />
+        <h1 className="m-4 text-center">{title}</h1>
+      </a>
     </article>
   );
 };
--- pages/articles/Articles.jsx
+++ pages/articles/Articles.jsx
@@ -1,9 +1,9 @@
 import Article from "./Article";
 const Articles = ({ news }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-11/12 mx-auto">
       {news.map((article, index) => (
-        <Article article={article} key={index} />
+        <Article article={article} key={index} id={index + 1} />
       ))}
     </div>
   );
