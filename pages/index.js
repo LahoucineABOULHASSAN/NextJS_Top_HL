38ebc1a0b7ec107b0291f1fc2b3200ace4aaf8d2
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2021-09-09 12:46:01 +0100

    update env file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Articles from "./articles/Articles";
-const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEXT_PUBLIC_MARVEL_PUBLIC_API_KEY}`;
+const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEXT_PUBLIC_TVMAZE_PUBLIC_API_KEY}`;
 
 export const getStaticProps = async () => {
   const res = await fetch(URL);
